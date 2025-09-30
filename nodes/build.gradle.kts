62f5dd9bb3af0cd8aa65a52955aeaf75f79ba236
AnChen_qua <101809860+Anchenqua@users.noreply.github.com>
2025-09-30 18:51:34 +0800

    Update build.gradle.kts

--- nodes/build.gradle.kts
+++ nodes/build.gradle.kts
@@ -92,8 +92,8 @@ dependencies {
 
     java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
     // spigot/paper api
-    paperweight.paperDevBundle("1.21.5-R0.1-SNAPSHOT") // contains 1.18.2 nms classes
-    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.1-R0.1-SNAPSHOT") // contains 1.18.2 nms classes
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 
     tasks {
         assemble {
