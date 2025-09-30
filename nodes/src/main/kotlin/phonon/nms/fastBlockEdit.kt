c39d5fda79752bf592bb3aece160f53171a95618
AnChen_qua <101809860+Anchenqua@users.noreply.github.com>
2025-09-30 20:43:21 +0800

    Update fastBlockEdit.kt

--- nodes/src/main/kotlin/phonon/nms/fastBlockEdit.kt
+++ nodes/src/main/kotlin/phonon/nms/fastBlockEdit.kt
@@ -1,36 +1,10 @@
 /**
  * Fast set blocks
  * Because default world.setBlock or block.setType is very slow (lighting + packets)
- * 
- * See: https://www.spigotmc.org/threads/how-to-set-blocks-incredibly-fast.476097/
- * 
- * ```
- * This is code I actually use.
- * The code itself is very clear.
- * What I do here is just simple 3 steps:
- * 1. modify block data in memory
- * 2. update lighting
- * 3. unload & load chunk data
- * 
- * Minecraft server contains data as of what chunks are loaded for players. However, it's not visible and we have to check whether a modified chunk is in view distance or not.
- * 
- * There are several cons that you have to take care of.
- * 1. You can modify blocks in unloaded chunks. However, it's slower than usual.
- * 2. Block update does not happen.
- * 3. Light update is not perfect when you edit unloaded chunks
- * 4. Light update doesn't work well when you have Sodium or some other lighting mod installed, because lighting is cached and ignore lighting update packet
- * ```
- * - Toshimichi
- * 
- * This is sufficiently different between 1.16.5 and 1.18.2: 
- * the chunk + lighting packet format changed. (Along with nms mapping
- * changes with de-obfuscated 1.17+ mappings.)
- * So just keep this separately implemented per version.
  */
 package phonon.nms.blockedit
 
-import java.util.HashMap
-import java.util.HashSet
+import java.util.*
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
@@ -41,6 +15,10 @@ import phonon.nodes.nms.NMSPacketLevelChunkWithLightPacket
 import phonon.nodes.nms.CraftWorld
 import phonon.nodes.nms.CraftPlayer
 import phonon.nodes.nms.CraftMagicNumbers
+
+// 1.21.1 特定的 import
+import net.minecraft.world.level.chunk.ChunkStatus
+import net.minecraft.server.level.ServerLevel
 
 public class FastBlockEditSession(
     val bukkitWorld: org.bukkit.World
@@ -55,7 +33,7 @@ public class FastBlockEditSession(
      * Mark a block as updated to a new material. This is lazy
      * and does not actually change world until `execute` is called.
      */
-    public fun setBlock(x: Int, y: Int, z: Int, material: Material) {
+    公共 fun setBlock(x: Int, y: Int, z: Int, material: Material) {
         modified.put(NMSBlockPos(x, y, z), CraftMagicNumbers.getBlock(material).defaultBlockState())
     }
 
@@ -74,23 +52,29 @@ public class FastBlockEditSession(
 
     /**
      * Run the block changes and send updates to players in view distance.
-     * 
-     * Chunk source api:
-     * https://nms.screamingsandals.org/1.18.2/net/minecraft/server/level/ServerChunkCache.html
+     * Updated for Minecraft 1.21.1
      */
-    public fun execute(updateLighting: Boolean) {
+    公共 fun execute(updateLighting: Boolean) {
         //modify blocks
         val chunks: HashSet<NMSChunk> = hashSetOf()
         for ( (bPos, bState) in modified ) {
-            val chunk = world.getChunkSource().getChunk(bPos.getX() shr 4, bPos.getZ() shr 4, 1)
+            // 1.21.1 正确的获取chunk方式
+            val chunkX = bPos.getX() shr 4
+            val chunkZ = bPos.getZ() shr 4
+            
+            // 方法1：使用 ChunkStatus (推荐)
+            val chunk = world.getChunkSource().getChunk(chunkX, chunkZ, ChunkStatus.FULL, true)
+            
             if ( chunk !== null ) {
                 chunks.add(chunk)
-                chunk.setBlockState(bPos, bState, 0)
+                // 1.21.1 中 setBlockState 可能需要不同的参数
+                // 第三个参数通常是 updateLight (boolean)
+                chunk.setBlockState(bPos, bState, false)
             }
         }
 
-        // update lighting
-        val lightingEngine = world.getChunkSource().getLightEngine()
+        // update lighting - 1.21.1 中的 Lighting 引擎
+        val lightingEngine = world.getLightEngine()
         if ( updateLighting ) {
             for ( pos in modified.keys ) {
                 lightingEngine.checkBlock(pos)
@@ -99,12 +83,14 @@ public class FastBlockEditSession(
 
         // send chunk updates to players
         for ( chunk in chunks ) {
-            val packetChunk = NMSPacketLevelChunkWithLightPacket(chunk, chunk.getLevel().getLightEngine(), null, null, true)
+            // 1.21.1 中的 Packet 创建方式
+            val packetChunk = NMSPacketLevelChunkWithLightPacket(chunk, world.getLightEngine(), null, null, true)
 
             for ( p in Bukkit.getOnlinePlayers() ) {
                 val nmsPlayer = (p as CraftPlayer).getHandle()
                 val dist = Bukkit.getViewDistance() + 1
                 val playerChunk = nmsPlayer.chunkPosition()
+                
                 if ( chunk.getPos().x < playerChunk.x - dist ||
                      chunk.getPos().x > playerChunk.x + dist ||
                      chunk.getPos().z < playerChunk.z - dist ||
